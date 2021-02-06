84b509a2da36210d1f981cf35006ca0c8d01d8f7
Thabiso Setefane <setefanesetefane@gmail.com>
2021-02-06 10:35:50 -0600

    create signup screen styles

---
+++ src/components/screens/signup/styles.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Dimensions, StyleSheet} from 'react-native';
+import {COLORS} from '../../../constants';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: 'center',
+  },
+  inputContainer: {
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    alignItems: 'center',
+    alignSelf: 'center',
+    height: 50,
+    borderRadius: 10,
+    margin: 10,
+    width: 300,
+  },
+  inputBox: {
+    width: 300,
+    left: 2,
+    height: 30,
+    marginVertical: 10,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255,255,255,0.2)',
+  },
+  buttonText: {
+    color: COLORS.white,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    alignSelf: 'center',
+    padding: 20,
+  },
+  passwordIcon: {
+    color: COLORS.blue,
+    right: 5,
+  },
+  buttons: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 25,
+    shadowOpacity: 0.3,
+    shadowColor: 'black',
+    shadowRadius: 25,
+    backgroundColor: COLORS.blue,
+    width: 120,
+    height: 40,
+  },
+});
