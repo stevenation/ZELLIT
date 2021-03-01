7e8839c8009cba903db68d679bbc32fc914d45c1
Thabiso Setefane <setefanesetefane@gmail.com>
2021-02-28 23:53:42 -0600

    Update index.js

--- src/components/screens/profile/index.js
+++ src/components/screens/profile/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import {Text, View} from 'react-native'
+import React, {useContext} from 'react'
+import {Text, TouchableOpacity, View} from 'react-native'
 import {SafeAreaView} from "react-native-safe-area-context";
+import {AuthContext} from "../../../navigation/AuthProvider";
 
 export default function Profile() {
+    const {logout} = useContext(AuthContext)
+
     return (
         <SafeAreaView>
             <Text style={{fontSize:30}}>Profile</Text>
+            <TouchableOpacity onPress={() => {
+                logout()
+            }}>
+                <Text>Log Out</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
