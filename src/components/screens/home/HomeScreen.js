f2ff56bfe08f93c0ed193f966aeaa29f8b6f01bd
Thabiso Setefane <setefanesetefane@gmail.com>
2021-03-08 02:47:49 -0600

    Update HomeScreen.js

--- src/components/screens/home/HomeScreen.js
+++ src/components/screens/home/HomeScreen.js
@@ -3,6 +3,9 @@ import TabNavigator from "../../../navigation/TabNavigator";
 import {createStackNavigator} from "@react-navigation/stack";
 import ItemScreen from "../feed/itemScreen";
 import CategoryScreen from "../feed/categoryScreen";
+import addConfirm from "../add/addConfirm";
+import SeeAll from "../feed/seeAll";
+import CacheImage from "../feed/cacheImage";
 
 
 const Stack = createStackNavigator()
@@ -10,9 +13,11 @@ const Stack = createStackNavigator()
 export default function HomeScreen() {
     return (
         <Stack.Navigator screenOptions={{headerShown: false}}>
-            <Stack.Screen name={'Home'} component={TabNavigator}/>
+            <Stack.Screen name={'Feed'} component={TabNavigator}/>
             <Stack.Screen name={'ItemScreen'} component={ItemScreen}/>
             <Stack.Screen name={'categoryScreen'} component={CategoryScreen}/>
+            <Stack.Screen name={'AddConfirm'} component={addConfirm}/>
+            <Stack.Screen name={"SeeAll"} component={SeeAll}/>
         </Stack.Navigator>
     )
 }
