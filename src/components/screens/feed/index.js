3d86e2cfc1ec72a7b46fe798cde6bda77fbdb091
Thabiso Setefane <setefanesetefane@gmail.com>
2021-03-08 02:47:28 -0600

    Update index.js

--- src/components/screens/feed/index.js
+++ src/components/screens/feed/index.js
@@ -1,5 +1,5 @@
-import React, {useContext, useEffect, useState} from 'react'
-import {FlatList, SafeAreaView, ScrollView, StatusBar, Text, TouchableOpacity, View} from 'react-native'
+import React from 'react'
+import {FlatList, RefreshControl, SafeAreaView, ScrollView, StatusBar, Text, TouchableOpacity, View} from 'react-native'
 import {SearchBar} from "react-native-elements";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import {styles} from "./styles";
@@ -7,7 +7,11 @@ import {COLORS} from "../../../constants";
 import ItemCell from "./itemCell";
 import database from "@react-native-firebase/database";
 import {firebase} from "@react-native-firebase/auth";
-import {AuthContext} from "../../../navigation/AuthProvider";
+import * as FileSystem from "expo-file-system"
+
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
 
 export const categories = [
     {
@@ -46,158 +50,208 @@ export const categories = [
         icon: "infinity"
     }
 ]
+export default class Feed extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            refresh: false,
+            userId: firebase.auth().currentUser.uid,
+            userData: [],
+            itemsData: [],
+            title:""
 
-export default function Feed({navigation}) {
-    const userId = firebase.auth().currentUser.uid
-    const [userData, setUserData] = useState('')
-    const [itemsData, setItemsData] = useState('')
-    const date = new Date()
-
-    function getUserData() {
-        database().ref(`Users/${userId}`)
-            .on('value', snapshot => {
-                setUserData(snapshot.val())
-                database().ref(`${snapshot.val()['college']}/Items`)
-                    .on('value', snp => {
-                        var lst = []
-                        snp.forEach((child => {
-                            var item = child.val()
-                            lst.push({
-                                key: child.key,
-                                name: item.name,
-                                price: item.price,
-                                uid: item.uid,
-                                condition: item.condition,
-                                brand: item.brand,
-                                description: item.description,
-                                img_url: item.img_url,
-                                category: item.category,
-                                payment_method: item.payment_method
-                            })
-                        }))
-                        setItemsData(lst)
-                    })
-            })
+        }
     }
-
-    function addItem() {
-        database().ref(`${userData['college']}/Items/${userData['uid']}${date.toTimeString()}`)
-            .set({
-                name: "Eraser",
-                price: "$2",
-                brand:"Staedler",
-                description: "Latex Free",
-                condition: "New",
-                category: "Stationery",
-                payment_method: "Cash App",
-                img_url: "https://firebasestorage.googleapis.com/v0/b/flash-chat-ios-13-7845d.appspot.com/o/images%2Fitems%2FScreen%20Shot%202021-02-22%20at%2010.25.44%20AM.png?alt=media&token=5d12ac68-6291-4747-8b18-d5b4b7837f27",
-                uid: "joYpScOIycN7cUSLoWYVzQZguv82"
-            })
-    }
-
-    function capitalize(str) {
+    capitalize(str) {
         return str.replace(/\w\S*/g, (w) =>
             (w.replace(/^\w/, (c) =>
                 c.toUpperCase())))
     }
 
-    useEffect(() => {
-        getUserData()
-    }, []);
+    onRefresh = () => {
+
+        this.setState({refresh: true});
+        this.fetchData()
+        wait(2000).then(() => this.setState({refresh: false}));
+    }
+
+    async fetchData() {
+
+        var userId = firebase.auth().currentUser.uid
+        const folder_info = await FileSystem.getInfoAsync(`${FileSystem.cacheDirectory}items`)
+        if (!folder_info.exists) {
+            try {
+                FileSystem.makeDirectoryAsync(`${FileSystem.cacheDirectory}items`)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        userId == null ? console.log("userID is empty") :
+            database().ref(`Users/${userId}`)
+                .on('value', async snapshot => {
+                    this.setState({userData: snapshot.val()})
+                    this.setState({title: this.capitalize(snapshot.val()['college'])})
+                    await database().ref(`${snapshot.val()['college']}/Items`)
+                        .on('value', snp => {
+                            var lst = []
+                            var count = 0
+                            snp.forEach((async child => {
+                                var item = child.val()
+                                const path = `${FileSystem.cacheDirectory}items/item${count}`
+                                count = count + 1
+                                const image = await FileSystem.getInfoAsync(path)
+                                var uri
+                                if (image.exists) {
+                                    uri = image.uri
+                                } else {
+                                    console.log("downloading")
+                                    try {
+                                        const newImage = await FileSystem.downloadAsync(item.img_url, path)
+                                        uri = newImage.uri
+                                    } catch (error) {
+                                        console.log(error)
+                                    }
+                                }
+                                await lst.push({
+                                    key: child.key,
+                                    path: uri,
+                                    name: item.name,
+                                    price: item.price,
+                                    uid: item.uid,
+                                    condition: item.condition,
+                                    brand: item.brand,
+                                    description: item.description,
+                                    img_url: item.img_url,
+                                    category: item.category,
+                                    payment_method: item.payment_method
+                                })
+                                this.setState({itemsData: lst})
+
+                            }))
+
+                        })
+                })
+    }
+
+    UNSAFE_componentWillMount() {
+        this.fetchData()
+    }
+    render() {
 
 
-    return (
-        <View>
-            <StatusBar barStyle={"dark-content"}/>
-            <SafeAreaView>
+        return (
+            <View>
+                <StatusBar barStyle={"dark-content"}/>
+                <SafeAreaView>
+                    <View style={{paddingVertical:5}}>
+                        <Text style={styles.collegeName}>{this.state.title}</Text>
+                    </View>
 
-                <Text style={styles.collegeName}>{userData.college}</Text>
-                {/*<TouchableOpacity onPress={() => logout()}>*/}
-                {/*    <Text>-</Text>*/}
-                {/*</TouchableOpacity>*/}
-                <View>
-                    <SearchBar
-                        searchIcon={{size: 24}}
-                        containerStyle={{backgroundColor: COLORS.white, borderTopWidth: 0}}
-                        round={true}
-                        lightTheme={true}
-                        placeholder={"search item"}
-                    />
-                </View>
-                <FlatList
-                    horizontal={true}
-                    data={categories}
-                    renderItem={({item}) => (
-                        <TouchableOpacity onPress={()=>navigation.navigate('categoryScreen', {itemsData:itemsData, name:item.name})} style={styles.categoryItem}>
-                            <View style={styles.catBackground}>
-                                <MaterialCommunityIcons name={item.icon} size={24} color={COLORS.white}/>
-                            </View>
-                            <Text numberOfLines={1}>{item.name}</Text>
-                        </TouchableOpacity>
-                    )}/>
+                    {/*<View>*/}
+                    {/*    <SearchBar*/}
+                    {/*        searchIcon={{size: 24}}*/}
+                    {/*        containerStyle={{backgroundColor: COLORS.white, borderTopWidth: 0}}*/}
+                    {/*        round={true}*/}
+                    {/*        lightTheme={true}*/}
+                    {/*        placeholder={"search item"}*/}
+                    {/*    />*/}
+                    {/*</View>*/}
 
-                <ScrollView style={{maxHeight: 600}}>
-                    <View>
-                        <View style={styles.section}>
-                            <Text style={styles.sectionTitle}>Recent</Text>
-                            <TouchableOpacity onPress={()=>navigation.navigate('categoryScreen', {itemsData:itemsData})}>
-                                <Text style={styles.seeAll}>See all</Text>
+                    <FlatList
+                        horizontal={true}
+                        data={categories}
+                        renderItem={({item}) => (
+                            <TouchableOpacity onPress={() => this.props.navigation.navigate('categoryScreen', {
+                                itemsData: this.state.itemsData,
+                                name: item.name
+                            })} style={styles.categoryItem}>
+                                <View style={styles.catBackground}>
+                                    <MaterialCommunityIcons name={item.icon} size={24} color={COLORS.white}/>
+                                </View>
+                                <Text numberOfLines={1}>{item.name}</Text>
                             </TouchableOpacity>
-
-                        </View>
-
-                        <FlatList horizontal={true} data={itemsData} renderItem={({item}) => (
-                            <ItemCell itemData={item} navigation={navigation}/>
                         )}/>
 
-                    </View>
-                    <View>
-                        <View style={styles.section}>
-                            <Text style={styles.sectionTitle}>Electronics</Text>
-                            <TouchableOpacity>
-                                <Text style={styles.seeAll}>See all</Text>
-                            </TouchableOpacity>
+                    <ScrollView
+                        style={{maxHeight: 600}}
+                        refreshControl={
+                            <RefreshControl
+                                refreshing={this.state.refresh}
+                                onRefresh={this.onRefresh}
+                            />
+                        }
+                    >
+                        <View>
+                            <View style={styles.section}>
+                                <Text style={styles.sectionTitle}>Recent</Text>
+                            </View>
+                            <FlatList horizontal={true} data={this.state.itemsData} renderItem={({item}) => {
+                                return (
+                                    <ItemCell itemData={item} navigation={this.props.navigation}/>
+                                )
+                            }}/>
+                        </View>
+                        <View>
+                            <View style={styles.section}>
+                                <Text style={styles.sectionTitle}>Electronics</Text>
+                                <TouchableOpacity
+                                    onPress={() => {
+                                        this.props.navigation.navigate('SeeAll', {
+                                            itemsData: this.state.itemsData,
+                                            name: "Electronics"
+                                        })
+                                    }
+                                    }>
+                                    <Text style={styles.seeAll}>See all</Text>
+                                </TouchableOpacity>
 
+                            </View>
+
+                            <FlatList horizontal={true} data={this.state.itemsData} renderItem={({item}) => {
+                                if (item.category === "Electronics") {
+                                    return (<ItemCell itemData={item} navigation={this.props.navigation}/>)
+                                }
+                            }}/>
+                        </View>
+                        <View>
+                            <View style={styles.section}>
+                                <Text style={styles.sectionTitle}>Stationery</Text>
+                                <TouchableOpacity
+                                    onPress={() => this.props.navigation.navigate('SeeAll',
+                                        {itemsData: this.state.itemsData, name: "Stationery"})}>
+                                    <Text style={styles.seeAll}>See all</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <FlatList horizontal={true} data={this.state.itemsData} renderItem={({item}) => {
+                                if (item.category === "Stationery") {
+                                    return (<ItemCell itemData={item} navigation={this.props.navigation}/>)
+                                }
+
+                            }}/>
                         </View>
 
-                        <FlatList horizontal={true} data={itemsData} renderItem={({item}) => {
-                            if (item.category === "Electronics") {
-                                return (<ItemCell itemData={item} navigation={navigation}/>)
-                            }
-                        }}/>
-                    </View>
-                    <View>
-                        <View style={styles.section}>
-                            <Text style={styles.sectionTitle}>Stationery</Text>
-                            <TouchableOpacity>
-                                <Text style={styles.seeAll}>See all</Text>
-                            </TouchableOpacity>
+                        <View>
+                            <View style={styles.section}>
+                                <Text style={styles.sectionTitle}>Textbooks</Text>
+                                <TouchableOpacity
+                                    onPress={() => this.props.navigation.navigate('SeeAll', {
+                                        itemsData: this.state.itemsData,
+                                        name: "Textbooks"
+                                    })}
+                                >
+                                    <Text style={styles.seeAll}>See all</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <FlatList horizontal={true} data={this.state.itemsData} renderItem={({item}) => {
+                                if (item.category === "Textbooks") {
+                                    return (<ItemCell itemData={item} navigation={this.props.navigation}/>)
+                                }
+                            }}/>
                         </View>
-                        <FlatList horizontal={true} data={itemsData} renderItem={({item}) => {
-                            // console.log(item.category)
-                            if (item.category === "Stationery") {
-                                return (<ItemCell itemData={item} navigation={navigation}/>)
-                            }
-
-                        }}/>
-                    </View>
-
-                    <View>
-                        <View style={styles.section}>
-                            <Text style={styles.sectionTitle}>Textbooks</Text>
-                            <TouchableOpacity>
-                                <Text style={styles.seeAll}>See all</Text>
-                            </TouchableOpacity>
-                        </View>
-                        <FlatList horizontal={true} data={itemsData} renderItem={({item}) => {
-                            if (item.category === "Textbooks") {
-                                return (<ItemCell itemData={item} navigation={navigation}/>)
-                            }
-
-                        }}/>
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
-        </View>
-    )
+                        <View style={{height: 50}}></View>
+                    </ScrollView>
+                </SafeAreaView>
+            </View>
+        )
+    }
 }
