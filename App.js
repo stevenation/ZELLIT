7a032d243f040e84f07594dcef5536562548ee2a
Thabiso Setefane <setefanesetefane@gmail.com>
2021-03-08 02:48:18 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,4 @@
 import React from 'react'
-import {SafeAreaView, Text} from 'react-native'
-import SignIn from "./src/components/screens/signin";
-import SignUp from "./src/components/screens/signup";
-import Feed from "./src/components/screens/feed";
-import ItemScreen from "./src/components/screens/feed/itemScreen";
 import Providers from "./src/navigation";
 
 export default function App() {
