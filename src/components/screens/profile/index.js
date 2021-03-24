6123da10c1578270c3b0de4c14c36e32dac66847
Thabiso Setefane <setefanesetefane@gmail.com>
2021-03-24 00:12:46 -0500

    show grid

--- src/components/screens/profile/index.js
+++ src/components/screens/profile/index.js
@@ -11,6 +11,10 @@ import {Rating, AirbnbRating} from 'react-native-ratings';
 import {COLORS} from '../../../constants';
 import {FlatGrid} from 'react-native-super-grid';
 import {Dimensions} from 'react-native';
+import {BackgroundImage} from 'react-native-elements/dist/config';
+import {DrawerActions} from '@react-navigation/routers';
+
+const WIDTH = Dimensions.get('screen').width;
 
 export default class Profile extends Component {
   _isMounted = false;
@@ -21,63 +25,124 @@ export default class Profile extends Component {
       userId: firebase.auth().currentUser.uid,
       userData: '',
       img_url: '',
+      itemsData: [],
     };
   }
 
   UNSAFE_componentWillMount() {
-    this._isMounted = true;
-    if (this._isMounted) {
-      database()
-        .ref(`Users/${this.state.userId}`)
-        .once('value', async (snapshot) => {
-          this.setState({userData: snapshot.val()});
-          var url = await storage()
-            .ref(`images/profile_pictures/${snapshot.val().profile_picture}`)
-            .getDownloadURL();
-          this.setState({img_url: url});
-        });
-    }
+    database()
+      .ref(`Users/${this.state.userId}`)
+      .once('value', async (snapshot) => {
+        this.Unsubscribe(snapshot.val().college);
+        this.setState({userData: snapshot.val()});
+        var url = await storage()
+          .ref(`images/profile_pictures/${snapshot.val().profile_picture}`)
+          .getDownloadURL();
+        this.setState({img_url: url});
+      });
   }
   componentWillUnmount() {
     this._isMounted = false;
+    database()
+      .ref(`${this.state.userData.college}/items`)
+      .off('value', this.Unsubscribe);
   }
 
-  
+  Unsubscribe(college) {
+    database()
+      .ref(`${college}/Items`)
+      .on('value', (snapshot) => {
+        var ls = [];
+        snapshot.forEach((child) => {
+          if (child.val().iud === firebase.auth().currentUser.iud) {
+            FastImage.preload([{uri: child.val().img_url}]);
+            ls.push({...child.val(), key: child.val().key});
+          }
+        });
+        this.setState({itemsData: ls});
+      });
+  }
   render() {
     return (
       <SafeAreaView>
         <Text style={{fontWeight: '700', fontSize: 24, alignSelf: 'center'}}>
           {this.state.userData.name}
         </Text>
-        <View
-          style={{
-            height: 200,
-            backgroundColor: COLORS.blue,
-            borderBottomLeftRadius: 50,
-            // borderBottomRightRadius: 50,
-            borderTopRightRadius: 50,
-            justifyContent: 'center',
-          }}>
-          <View style={{flexDirection: 'row'}}>
-            <FastImage
-              style={styles.imageRectangular}
-              source={{
-                uri: this.state.img_url,
-              }}
-            />
-            <View style={{marginTop: 10}}>
-              <Text style={{fontWeight: '500', fontSize: 14}}>
-                {this.state.userData.college}
-              </Text>
-              <AirbnbRating
-                type="star"
-                showRating={false}
-                fraction={5}
-                defaultRating={this.state.userData.rating}
-                isDisabled={true}
-                size={20}
+        <View>
+          <View
+            style={{
+              position: 'relative',
+              height: 200,
+              backgroundColor: COLORS.blue,
+              borderBottomLeftRadius: 50,
+              borderTopRightRadius: 50,
+              justifyContent: 'center',
+              top: 0,
+              left: 0,
+            }}>
+            <View style={{flexDirection: 'row'}}>
+              <FastImage
+                style={styles.imageRectangular}
+                source={{
+                  uri: this.state.img_url,
+                }}
               />
-              <Text>{this.state.userData.trades} trades</Text>
+              <View style={{marginTop: 10}}>
+                <Text style={{fontWeight: '500', fontSize: 14}}>
+                  {this.state.userData.college}
+                </Text>
+                <AirbnbRating
+                  type="star"
+                  showRating={false}
+                  fraction={5}
+                  defaultRating={this.state.userData.rating}
+                  isDisabled={true}
+                  size={20}
+                />
+                <Text>{this.state.userData.trades} trades</Text>
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              height: 100,
+              width: WIDTH,
+              borderBottomLeftRadius: 50,
+              borderBottomWidth: 10,
+              borderLeftWidth: 1,
+              borderColor: COLORS.blue,
+              position: 'absolute',
+              bottom: -40,
+              left: 0,
+              // backgroundColor: 'black',
+            }}>
+            <View
+              style={{
+                bottom: -65,
+                left: 0,
+
+                flexDirection: 'row',
+                justifyContent: 'space-evenly',
+                alignItems: 'center',
+              }}>
+              <TouchableOpacity
+                onPress={() => {
+                  this.props.navigation.navigate('Buying');
+                }}>
+                <Text>Buying</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  this.props.navigation.navigate('Selling');
+                }}>
+                <Text>Selling</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  this.props.navigation.navigate('CachedImageExample');
+                }}>
+                <Text>Cache</Text>
+              </TouchableOpacity>
             </View>
           </View>
         </View>
@@ -87,37 +152,78 @@ export default class Profile extends Component {
           }}>
           <Text>Log Out</Text>
         </TouchableOpacity>
-        <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.navigation.navigate('Buying');
-            }}>
-            <Text>Buying</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.navigation.navigate('Selling');
-            }}>
-            <Text>Selling</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.navigation.navigate('CachedImageExample');
-            }}>
-            <Text>Cache</Text>
-          </TouchableOpacity>
-        </View>
+
         <FlatGrid
+          style={{height: 430, marginTop: 25}}
           itemDimension={Dimensions.get('screen').width / 2.5}
-          data={[1, 2, 3, 4, 5, 6]}
-          renderItem={({item}) => (
-            <View style={{backgroundColor: 'blue'}}>
-              <Text>{item}</Text>
-            </View>
-          )}
+          data={this.state.itemsData}
+          renderItem={({item}) => ItemCell(item)}
         />
+        <View styles={{height: 100}}></View>
       </SafeAreaView>
     );
   }
 }
-
+function ItemCell(data) {
+  let source = {uri: data.img_url, priority: FastImage.priority.high};
+  return (
+    <TouchableOpacity style={{height: 130}}>
+      <BackgroundImage
+        source={{uri: data.img_url}}
+        style={{
+          height: 130,
+          width: WIDTH / 2.2,
+          position: 'relative', // because it's parent
+          top: 0,
+          left: 0,
+        }}
+      />
+      {data.sold && (
+        <View
+          style={{
+            width: 60,
+            padding: 10,
+            height: 40,
+            position: 'absolute',
+            top: 0, // position where you want
+            right: 0,
+            justifyContent: 'center',
+            borderTopLefttRadius: 20,
+            borderBottomLeftRadius: 20,
+            backgroundColor: COLORS.green,
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              fontWeight: 'bold',
+              color: 'white',
+            }}>
+            SOLD
+          </Text>
+        </View>
+      )}
+      <View
+        style={{
+          maxWidth: 90,
+          padding: 10,
+          height: 40,
+          position: 'absolute',
+          bottom: 0, // position where you want
+          left: 0,
+          justifyContent: 'center',
+          borderTopRightRadius: 20,
+          borderBottomRightRadius: 20,
+          backgroundColor: COLORS.blue,
+          alignItems: 'center',
+        }}>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            color: 'white',
+          }}>
+          ${data.price}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
